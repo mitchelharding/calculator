06b0a7bfd81d85f76aec6f0f1e3917b05dc9ded1
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-06 12:52:24 -0700

    Added more JS rules for muiltipy, divide, subtract

--- js/script.js
+++ js/script.js
@@ -6,4 +6,29 @@ var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
 
 
-alert(add(number1, number2));
+var subtract = function(number1, number2){
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter a number:"));
+
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter a number"));
+
+
+var divide = function(number1, number2){
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("ENter a number"));
+var number2 = parseInt(prompt("Enter a number"));
+
+
+var result = add(number1, number2);
+alert(result);
